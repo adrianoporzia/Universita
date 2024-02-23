9b70f038e1b63173d358a82c36faba74f6450b95
adriano <porziaadriano@gmail.com>
2024-02-23 16:43:07 +0100

    ord

---
+++ Algoritmi/programmazione_c/Esercizi.c/union.py
@@ -0,0 +1,22 @@
+
+
+
+# %%
+#intersection
+def union(d0,d1):
+    l=[]
+    for k in d0.keys():
+        if k in l:
+            continue
+        else:
+            l.append(k)
+    for k in d1.keys():
+        if k in l:
+            continue
+        else:
+            l.append(k)
+    return l
+d0={'prova':6, 'salve ':'codice', 6:3.14}
+d1={'prova1':6, '3.14':'(1,2)', 55:3.14}
+print(union(d0,d1))
+# %%
